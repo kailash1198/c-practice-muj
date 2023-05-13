4235c8c20161a942c11cd1b10f7696ce22bc04e4
kailash kumar <kailash414243@gmail.com>
2023-05-13 23:44:02 +0530

    Variable and data types practice in c language

---
+++ variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("Variable Learning and Understanding\n\n");
+    // int myNumOne = 10;
+    // char myFirstLetter = "K";
+    // printf("My num is : %d \n\n", myNumOne);
+    // printf("My First Letter is : %c \n\n", myFirstLetter);
+
+
+    int myNumber = 163653;
+    printf("%d", myNumber);
+    return 0;
+}
