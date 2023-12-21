c1aed079c873a106a63b5bd946a7487e4395573f
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2023-12-21 17:19:37 +0000

    data type practice

--- practice.c
+++ practice.c
@@ -1,4 +1,4 @@
-#include <stdio.h>                 //library standard i/o
+#include <stdio.h> //library standard i/o
 // int addNum(int numOne, int numTwo) // numOne and numTwo are parameters
 // {
 //     int totalNum = numOne + numTwo;
@@ -16,4 +16,24 @@ int main() // declare main function
     // const int userAge = 100;
     // userAge = 20; //got error if this line code executed after complile the code
     // printf("User age is = %d\n", userAge);
+
+    // int numOne = 10;
+    // short int myNum = 20;
+    // long int myBigNum = 386464578;
+    // signed int numTwo = 2;
+    // unsigned int numThree = 10;
+    // int sizeStore = sizeof numOne;
+    // int sizeStore = sizeof myNum;
+    // int sizeStore = sizeof myBigNum;
+    // int sizeStore = sizeof numTwo;
+    // int sizeStore = sizeof numThree;
+    // printf("\n\n %d \n\n", sizeStore);
+
+    // signed int numOne = 10;
+    // unsigned int numTwo =-200;
+//error beacuse you cannot use negative in unsigned
+    // printf("\n Signed int memory size in bytes: %d\n", numOne);
+    // printf("\n Un Signed int memory size in bytes: %u\n", numTwo);
+
+    return 0;
 }
