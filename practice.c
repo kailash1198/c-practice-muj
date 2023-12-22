3fa7738c324fa18d9f7520efff4a49f3c03ecfe2
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2023-12-22 14:45:04 +0000

    literal practice

--- practice.c
+++ practice.c
@@ -31,9 +31,43 @@ int main() // declare main function
 
     // signed int numOne = 10;
     // unsigned int numTwo =-200;
-//error beacuse you cannot use negative in unsigned
-    // printf("\n Signed int memory size in bytes: %d\n", numOne);
-    // printf("\n Un Signed int memory size in bytes: %u\n", numTwo);
+    // error beacuse you cannot use negative in unsigned
+    //  printf("\n Signed int memory size in bytes: %d\n", numOne);
+    //  printf("\n Un Signed int memory size in bytes: %u\n", numTwo);
 
+    // short int numOne = 10;
+    // int sizeCheck = sizeof numOne;
+    // printf("\n\n %d \n\n", sizeCheck);
+
+    //   long int numOne = 10;
+    // int sizeCheck = sizeof numOne;
+    // printf("\n\n %d \n\n", sizeCheck);
+
+    // int numOne = 10;
+    // int sizeCheck = sizeof numOne;
+    // printf("\n\n %d \n\n", sizeCheck);
+
+    // long double numOne = 10;
+    // int sizeCheck = sizeof numOne;
+    // printf("\n\n %d \n\n", sizeCheck);
+
+    // short int numOne = 89654;
+    // printf("\n\n%d\n\n", numOne);
+
+    // int decimal = 42;
+    // int octal = 052;
+
+    // float sci = 1.2e-3;
+    // printf("\n\n %f \n\n", sci);
+
+    // const int octalNumbers = 0567;
+    // const int octalNumbersTwo = 056457;
+    // printf("\n\n %d \n\n", octalNumbers);
+    // printf("\n\n %d \n\n", octalNumbersTwo);
+
+    const int hexaNumbers = 0x567;
+    const int hexaNumbersTwo = 0x56457;
+    printf("\n\n %d \n\n", hexaNumbers);
+    printf("\n\n %d \n\n", hexaNumbersTwo);
     return 0;
 }
