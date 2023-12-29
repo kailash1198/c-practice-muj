1da24246a48a37be9268269731b5ce70f99a6a54
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2023-12-29 17:18:56 +0000

    function practice

---
+++ function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// ===== function ==== 
+int wordChecker(char userWord[29]){
+for(int i=0; i<=strlen(userWord); i++ ){
+    strch(userWord);
+}
+}
+
+// ==== main function ====== 
+int main(){
+    char userParagraph[300];
+    printf("\n\nPlease enter your Paragraph: \n");
+    gets(userParagraph);
+    system("clear");
+    puts(userParagraph);
+    return 0;
+}
