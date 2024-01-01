18f5d4ab6448ea6fbfb517edd13dad73b350c57c
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-01 14:11:58 +0000

    pointer practice in c

---
+++ pointer.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    // int numOne = 100;
+    // int *numTwo = &numOne;
+    // int numThree =numTwo;
+    // printf("\nThe address of memory location is : %p\n\n", &numTwo);
+    // printf("\n\n%d\n\n", numThree);
+    // ============ POINTER TO POINTER ==========
+    // int numOne = 10;
+    // int *pointerOne = &numOne;
+    // int **pointerTwo = &pointerOne;
+    // int ***pointerThree = &pointerTwo;
+    // int ****pointerFourth = &pointerThree;
+
+    // printf("\n");
+    // printf("Pointer one adrress is: %p\n", pointerOne);
+    // printf("Pointer two adrress is: %p\n", pointerTwo);
+    // printf("Pointer three adrress is: %p\n", pointerThree);
+    // printf("Pointer fourth adrress is: %p\n", pointerFourth);
+
+    // =================== POINTER ARITHMATIC ===========
+    // int numOne = 200;
+    // int *pointerNumOne = &numOne;
+    // printf("\n");
+    // printf("%p\n", pointerNumOne);
+    // pointerNumOne++;
+    // printf("\n");
+    // printf("%p\n", pointerNumOne);
+
+    // int myArray[5] = {100, 200, 300, 400, 500};
+    // int *arrayPointerOne = &myArray[0];
+    // printf("\n");
+    // printf("The myArray[0] is : %d\n", *arrayPointerOne);
+    // arrayPointerOne++;
+    // printf("The myArray[1] is : %d\n", *arrayPointerOne);
+    // arrayPointerOne++;
+    // printf("The myArray[2] is : %d\n", *arrayPointerOne);
+
+    // for(int i=0; i<5; i++){
+    //     int *arrayPointerOne = &myArray[i]; 
+    //     printf("The myArray[0] is : %d\n", *arrayPointerOne);
+    // }
+
+
+
+    return 0;
+}
