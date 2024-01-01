88e5dea5ebb400a0100735ade68cf65c8bb71ef7
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-01 03:02:29 +0000

    function learning and practice

--- function.c
+++ function.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-// ===== function ==== 
-int wordChecker(char userWord[29]){
-for(int i=0; i<=strlen(userWord); i++ ){
-    strch(userWord[i]);
-}
-}
+// ===== function ====
+// int wordChecker(char userWord[29]){
+// for(int i=0; i<=strlen(userWord); i++ ){
+//     strch(userWord[i]);
+// }
+// }
 
-// ==== main function ====== 
-int main(){
-    char userParagraph[300];
-    char userWord[20];
-    printf("\n\nPlease enter your Paragraph: \n");
-    gets(userParagraph);
-    system("clear");
-    puts(userParagraph);
+// float noReturnFunc(float numOne)
+// {
+//     (numOne*100) / (1 * 100);
+//     return numOne;
+// }
 
-    printf("\nEnter word you want check: ");
-    scanf("%s", userWord);
+// ==== main function ======
+int main()
+{
+    // char userParagraph[300];
+    // char userWord[20];
+    // printf("\n\nPlease enter your Paragraph: \n");
+    // gets(userParagraph);
+    // system("clear");
+    // puts(userParagraph);
 
-    if(userWord!=0){
-        wordChecker(userWord);
-    }
+    // printf("\nEnter word you want check: ");
+    // scanf("%s", userWord);
+
+    // if(userWord!=0){
+    //     wordChecker(userWord);
+    // }
+
+    // float PercentageResult = noReturnFunc(9.876);
+    // printf("\nThe percentage value is : %f\n", PercentageResult);
+    // char line[100];
+    // char *nextLine;
+    // while (getline(line, sizeof(line), stdin))
+    // {
+    // }
+
+
+    char userLine[100];
+    printf("\nPlease enter 2 line: ");
+    fgets(userLine, 100, stdin);
+
     return 0;
 }
