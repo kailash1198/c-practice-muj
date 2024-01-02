07cbb5648e14e003d318b0fb43c2ffe5662362b2
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-02 15:02:09 +0000

    array practice

---
+++ array-and-string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    // int arrayOne[2]={10,11};
+    // int *pointerArray[2] = &arrayOne;
+    // printf("\n");
+    // printf("%d", arrayOne[1]);
+    // printf("\n");
+
+    // int sizeChecking = sizeof(arrayOne);
+    // printf("%d\n", sizeChecking);
+
+    // char myLetter[3] = {'a', 'b', 'c'};
+    // int charSizeChecking = sizeof(myLetter);
+    // printf("\n%d\n", charSizeChecking);
+
+    // char myVar='a';
+    // int myVarSizeChecking = sizeof(myVar);
+    // printf("\n%d\n", myVarSizeChecking);
+
+    // char myString[10] = "name";
+    // printf("\n%s\n", myString);
+    // int stringSizeChecking = sizeof(myString);
+    // printf("\n%d\n", stringSizeChecking);
+
+    
+    return 0;
+}
