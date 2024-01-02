612e1aa164bff12cd9dc8ce64550bbd354a4b758
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-02 16:27:42 +0000

    array practice

--- array-and-string.c
+++ array-and-string.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+// int arrayFunction(int myArray[10]){
+//     for(int i=0; i<10;i++){
+//         printf("%d\n", myArray[i]);
+//     }
+// }
+
+// void myArrayFunction(int age1, int age2)
+// {
+//     printf("\n%d\n", age1);
+//     printf("\n%d\n", age2);
+// }
+int main()
+{
     // int arrayOne[2]={10,11};
     // int *pointerArray[2] = &arrayOne;
     // printf("\n");
@@ -24,6 +36,65 @@ int main(){
     // int stringSizeChecking = sizeof(myString);
     // printf("\n%d\n", stringSizeChecking);
 
-    
+    // int arraySize;
+    // printf("\n");
+    // printf("Please tell me array size : ");
+    // scanf("%d", &arraySize);
+    // int myArray[arraySize];
+    // printf("You are decided the size is : %d\n", arraySize);
+
+    // printf("\n");
+    // printf("Please enter content: ");
+    // for (int i = 0; i < arraySize; i++)
+    // {
+    //     scanf("\n%d\n", &myArray[i]);
+    // }
+
+    // for (int j = 0; j < arraySize; j++)
+    // {
+    //     printf("\nThe size of array is = %d", myArray[j]);
+    // }
+
+    // int multiDimeArray[2][3];
+    // multiDimeArray[0][0]=10;
+    // multiDimeArray[0][1]=20;
+    // multiDimeArray[0][2]=30;
+    // multiDimeArray[0][3]=40;
+    // multiDimeArray[0][4]=50;
+    // printf("\n%d\n", multiDimeArray[0][0]);
+    // printf("\n%d\n", multiDimeArray[0][1]);
+    // printf("\n%d\n", multiDimeArray[0][2]);
+    // printf("\n%d\n", multiDimeArray[0][3]);
+    // printf("\n%d\n", multiDimeArray[0][4]);
+
+    // int multiDimeArray[2][3] = {3, 4, 5,
+    //                             6, 7, 8};
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         printf("%d\n", multiDimeArray[i][j]);
+    //     }
+    // }
+
+    // int multiDimensionalArray[3][5][10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150};
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+    //         for (int k = 0; k < 10; k++)
+    //         {
+    //             printf("%d\t", multiDimensionalArray[i][j][k]);
+    //         }
+    //     }
+    // }
+
+    // int multidimenstionalArraySizeChecking = sizeof(multiDimensionalArray);
+    // printf("\n%d\n", multidimenstionalArraySizeChecking);
+
+    // arrayFunction(10);
+    // int ageUser[2] = {20, 25};
+    // myArrayFunction(ageUser[0], ageUser[1]);
     return 0;
 }
