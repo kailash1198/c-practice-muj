0ca86a1ee4d92a83b8ce1045ff206fc269146bb9
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-03 16:49:55 +0000

    structure practice

--- structure.c
+++ structure.c
@@ -4,19 +4,37 @@
 int main()
 {
 
-    struct studentData
-    {
-        char studentName[30];
-        int studentAge;
-        int studentMarks;
-        char studentAddress[50];
-    };
-    strcpy(studentDetails.studentName, "Kailash Kumar");
-    struct studentData studentDetails;
-    studentDetails.studentAge = 25;
-    studentDetails.studentMarks = 98;
-    strcpy(studentDetails.studentAddress, "Bengaluru");
-    printf("\n%s\n", studentDetails.studentName);
+    // struct studentData
+    // {
+    //     char studentName[30];
+    //     int studentAge;
+    //     int studentMarks;
+    //     char studentAddress[50];
+    // };
+    // strcpy(studentDetails.studentName, "Kailash Kumar");
+    // struct studentData studentDetails;
+    // studentDetails.studentAge = 25;
+    // studentDetails.studentMarks = 98;
+    // strcpy(studentDetails.studentAddress, "Bengaluru");
+    // printf("\n%s\n", studentDetails.studentName);
+
+    // struct inputField
+    // {
+    //     char date[20];
+    //     char companyName[50];
+    //     char itemsName[200];
+    //     int qty;
+    //     char driverName[50];
+    // };
+    // struct inputField inputData;
+    // char userDate[30];
+
+    // printf("\nPlease enter Date: ");
+    // scanf("%s", userDate);
+    // fgets(userDate, 50, stdin);
+    // strcpy(inputData.date, userDate);
+    // printf("\n%s", inputData.date);
+    // printf("%s", userDate);
 
     return 0;
 }
