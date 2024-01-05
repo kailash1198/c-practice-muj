5c1a0693c1f32fd3a5d655baf3b723c8e31f54c0
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-05 01:49:18 +0000

    variadic function practice

--- function.c
+++ function.c
@@ -1,3 +1,4 @@
+#include <stdarg.h> // for access variadic function
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -68,11 +69,80 @@
 //     (*pointerOne)();
 // }
 
+// ====== VARIADIC FUNCTION ======
+// int AddNumbers(int n, ...)
+// {
+//     int Sum = 0;
+//     va_list ptr;
+//     va_start(ptr, n);
+//     for (int i = 0; i < n; i++)
+//         Sum += va_arg(ptr, int);
+//     va_end(ptr);
+//     return Sum;
+// }
 
+// int variadicFunction(int numArgumentOne, int numArgumentTwo, ...)
+// {
+//     int totalNum = 0;
+
+//     va_list listArgument;
+//     va_start(listArgument, numArgumentTwo);
+
+//     for (int i = 0; i < numArgumentOne; i++)
+//     {
+//         totalNum += va_arg(listArgument, int);
+//     }
+
+//     va_end(listArgument);
+// }
+
+// int sumNum(int num, ...)
+// {
+//     int sum = 0;
+//     va_list listVar;
+//     va_start(listVar, num);
+//     for (int i = 0; i < num; i++)
+//     {
+//         int n = va_arg(listVar, int);
+//         sum = sum+n;
+//     }
+//     va_end(listVar);
+//     return sum;
+// }
+
+// int substractFunc(int num, ...)
+// {
+//     int subtract=0;
+//     va_list argumentList;
+//     va_start(argumentList, num);
+//     for (int i = 0; i < num; i++)
+//     {
+//         int argumentStore = va_arg(argumentList, int);
+//         subtract = subtract -(argumentStore);
+//     }
+//     va_end(argumentList);
+//     return subtract;
+// }
 
 // ==== main function ======
 int main()
 {
+
+    // printf("\n%d\n", substractFunc(2, 10, 20));
+    // printf("\n%d\n", sumNum(3,100,10, 100));
+
+    // printf("\n%d\n", variadicFunction(10, 20, 30));
+    // printf("\n\n Variadic functions: \n");
+    // printf("\n 1 + 2 = %d ",
+    //        AddNumbers(2, 1, 2));
+
+    // printf("\n 3 + 4 + 5 = %d ",
+    //        AddNumbers(3, 3, 4, 5));
+
+    // printf("\n 6 + 7 + 8 + 9 = %d ",
+    //        AddNumbers(4, 6, 7, 8, 9));
+
+    // printf("\n");
 
     // myFunc(&funcTwo);
     // callBackFunc(&funcTwo);
