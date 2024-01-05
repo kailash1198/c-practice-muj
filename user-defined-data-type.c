dc53134e1a4597c0077df4e706fd93959fc0664e
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-05 16:36:44 +0000

    user defined data type learning and practice

--- user-defined-data-type.c
+++ user-defined-data-type.c
@@ -4,20 +4,77 @@
 
 int main()
 {
-    struct structureOne
-    {
-        /* data */
-        char userName[20];
-        int userAge;
-        int userCode;
-        int userIP;
-    };
+    // struct structureOne
+    // {
+    //     /* data */
+    //     char userName[20];
+    //     int userAge;
+    //     int userCode;
+    //     int userIP;
+    // };
 
-    struct structureOne userData;
-    userData.userName = strcpy("kailash");
-
+    // struct structureOne userData;
+    // strcpy(userData.userName, "kailash");
+    // userData.userAge = 19;
     // char *studentName[40] =;
     // *studentName = "Kailash";
     // printf("\n%s\n", *studentName);
+
+    // struct userDetails
+    // {
+    //     int serialNumber[10];
+    //     int subjectMarks[10];
+    // };
+    // struct userDetails user;
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     user.serialNumber[i] = i;
+    //     printf("%d - %d\n", user.serialNumber[i], i);
+    // }
+    // printf("%d\n", user.serialNumber);
+
+    // typedef int numOne;
+    // typedef int numTwo;
+
+    // numOne myNum = 100;
+    // numTwo yourNum = 200;
+
+    // typedef char string;
+    // string firstName[20] = "Kailash";
+    // string lastName[20] = "kumar";
+
+    // printf("\n%s\n", firstName);
+    // printf("\n%s\n", lastName);
+
+    // union studentDetails
+    // {
+    //     /* data */
+    //     char userName[10];
+    //     int myAge;
+    //     int userCode;
+    // };
+
+    // union studentDetails userData;
+
+    // enum weekDays{
+    //     monday,
+    //     tuesday,
+    //     wednesday,
+    //     thursday,
+    //     friday,
+    //     saturday,
+    //     sunday
+    // };
+
+    // enum weekDays today, tomorrow;
+    // today = friday;
+    // tomorrow = saturday;
+
+    // if(today==friday){
+    //     printf("\nToday is friday so i have to lots of work for completing my task and goal");
+    // }else{
+    //     printf("Erro");
+    // }
+    
     return 0;
 }
