297f9f6e8ade5add8d7034cd352ff997ef0982df
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-06 10:27:18 +0000

    enum practcie

--- enum.c
+++ enum.c
@@ -1,15 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    enum enumData{
-       one,
-       two,
-       three,
-       four
-    };
+int main(void)
+{
+    // enum enumData{
+    //    one,
+    //    two,
+    //    three,
+    //    four
+    // };
 
-    enum enumData enumVar;
+    // enum enumData enumVar;
+    // enumVar = one;
+
+    // if(enumVar==one){
+    //     printf("Enum is 1\n");
+    // }else{
+    //     printf("Please enter correct values\n");
+    // }
+    // char userEnterDays[30];
+    // enum weekDaysName
+    // {
+    //     monday,
+    //     tuesday,
+    //     wednesday,
+    //     thursday,
+    //     friday,
+    //     saturday,
+    //     sunday
+    // };
+
+    // enum weekDaysName todayDays;
+    // todayDays = saturday;
+    // printf("Please enter days Name: ");
+    // fgets(userEnterDays, 20, stdin);
+    // printf("%s", userEnterDays);
+
+    // if (strcmp(userEnterDays, "monday") == 0)
+    // {
+    //     todayDays=monday;
+
+    //     switch (todayDays)
+    //     {
+    //     case monday:
+    //         printf("Today is monday\n");
+    //         break;
+    //     case tuesday:
+    //         printf("Today is tuesday\n");
+    //         break;
+    //     case wednesday:
+    //         printf("Today is wednesday\n");
+    //         break;
+    //     case thursday:
+    //         printf("Today is thursday\n");
+    //         break;
+    //     case friday:
+    //         printf("Today is friday\n");
+    //         break;
+    //     case saturday:
+    //         printf("Today is saturday\n");
+    //         break;
+    //     case sunday:
+    //         printf("Today is sunday\n");
+    //         break;
+    //     default:
+    //         printf("Please valid week days\n");
+    //         break;
+    //     }
+    // }
 
     
 }
