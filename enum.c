3c99e90117e2c4191c253f2fbdca1e17f0e6fef8
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-06 11:41:27 +0000

    ENUM PRACTICE

--- enum.c
+++ enum.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(void)
+int main()
 {
     // enum enumData{
     //    one,
@@ -69,5 +70,36 @@ int main(void)
     //     }
     // }
 
-    
+    /* Create a program that uses a union to represent a person's contact information. The contact information should include either a phone number (as an integer) or an email address (as a string). The program should have a menu that allows the user to input either a phone number or an email address for a person. Use a union to store the contact information, and include a flag in the union to indicate whether the information is a phone number or an email address. Implement functions to input, display, and update the contact information. Ensure that the program handles both types of information correctly based on the flag.*/
+    int mobileNum[12];
+    char email[50];
+    int option, b, c;
+    enum contactInformation
+    {
+        mobileNumber,
+        emailAddress
+    } infoDetails;
+
+    printf("Please enter details as per menu\n");
+    printf("1. Mobile number\n2. Email Address\n");
+    printf("Please select option what you want store: ");
+    scanf("%d", &option);
+    if (option == 1)
+    {
+        infoDetails = mobileNumber;
+        printf("\nPlease enter mobile Number: ");
+        for (int i = 0; i < 13; i++)
+        {
+            scanf("%d", &mobileNum[i]);
+        }
+    }
+    else if (option == 2)
+    {
+        infoDetails = emailAddress;
+        printf("Please enter Email address: ");
+        // fgets(email, 50, stdin);
+        scanf("%s", email);
+    }
+
+    return 0;
 }
