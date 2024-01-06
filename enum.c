79550fbc525b53a47e423be536aab17b9f5e634c
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-06 07:43:48 +0000

    enum practice

---
+++ enum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    enum enumData{
+       one,
+       two,
+       three,
+       four
+    };
+
+    enum enumData enumVar;
+
+    
+}
