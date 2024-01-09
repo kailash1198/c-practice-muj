4d4d82b08c3fd308c7b5a3f1a6575b266c2ec70a
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-09 01:55:45 +0000

    storage class practice in C

---
+++ storage-class.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int numOne;
+
+int main()
+{
+    // int numOne;
+    // printf("%d\n", numOne);
+    // int a = 10, i;
+    // printf("%d ", ++a);
+    // {
+    //     int a = 20;
+    //     for (i = 0; i < 3; i++)
+    //     {
+    //         printf("%d ", a); // 20 will be printed 3 times since it is the local value of a
+    //     }
+    // }
+    // printf("%d ", a);
+
+    // static int numTwo;
+    // printf("\n%d\n", numTwo);
+
+    // register int registerVar;
+    // printf("\n%d\n", registerVar);
+
+    // extern int numOne;
+    // printf("\n%d\n", numOne);
+    return 0;
+}
