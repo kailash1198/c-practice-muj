13056d4d3154c8971a538e1821eb68797cbcb4f5
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-15 14:28:30 +0000

    file handling practice

--- file-handling.c
+++ file-handling.c
@@ -1,13 +1,51 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+int main()
+{
     // printf("\n\nCheck\n\n");
 
-    FILE *myFileAddress = (int*)fopen("test.txt", "w");
-    fputs("This is file text Okay \n", myFileAddress);
-    myFileAddress = fputs("Done Kailash, Good Job", myFileAddress);
+    // FILE *myFileAddress = (int*)fopen("test.txt", "w");
+    // fputs("This is file text Okay \n", myFileAddress);
+    // myFileAddress = fputs("Done Kailash, Good Job", myFileAddress);
+
+    int option;
+    char userIntro[100];
+    
+    printf("Please tell me what you want : ");
+    printf("\n");
+    printf("1. Write simple intro text\n2. Enter BioData\n3. Enter your story\n\n");
+    printf("Tell me what you want to write: ");
+    scanf("%d", &option);
+    if (option == 1)
+    {
+        char inputText[100];
+        FILE *introAddress;
+        introAddress = fopen("intro.txt", "r");
+        fgets(userIntro, 100, introAddress);
+        system("clear");
+        printf("%s\n", userIntro);
+        fclose(introAddress);
+        printf("\n\nEnter input text which you want add in text file: ");
+        fgets(inputText, 100, stdin);
+        // scanf("%s", inputText);
+        introAddress = fopen("intro.txt", "a+");
+        fputs(inputText, introAddress);
+    }
+    else if (option == 2)
+    {
+        FILE *bioDataAddress = fopen("biodata.txt", "w");
+    }
+    else if (option == 3)
+    {
+        FILE *storyAddress = fopen("story.txt", "w");
+    }
+    else
+    {
+        system("clear");
+        printf("\nPlease enter valid option to write in file\n");
+    }
     return 0;
 }
