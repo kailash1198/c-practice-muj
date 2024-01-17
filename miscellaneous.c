2d2be69239473b09d218c64697c2a3ce2a7b0f2e
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-17 02:08:41 +0000

    practice

---
+++ miscellaneous.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    typedef int integerType;
+    integerType numOne = 20;
+    printf("%d\n", numOne);
+    return 0;
+}
