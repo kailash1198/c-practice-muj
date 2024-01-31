1434a242032ca979fcb5982e670217b860371e97
kailash kumar <kailash414243@gmail.com>
2024-01-31 08:06:28 +0530

    c problem practice

---
+++ problem-practice.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+// void swap(int *a, int *b){
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+
+// int findStrSize(char *string){
+//     int length = 0;
+//     while(*string!='\0'){
+//         length++;
+//         string++;
+//     }
+//     return length;
+// }
+
+// int reverseStr(char *str)
+// {
+//     int length = strlen(str);
+//     char *start = str;
+//     char *end = str + length - 1;
+
+//     while(start<end){
+//         char temp = *start;
+//         *start = *end;
+//         *end = temp;
+//         start++;
+//         end--;
+//     }
+// }
+
+int main()
+{
+    // Swap two variables using pointers.
+    // int numOne=10, numTwo=20;
+
+    //  swap(&numOne, &numTwo);
+    // printf("Numone = %d\n", numOne);
+    // printf("Numtwo = %d\n", numTwo);
+
+    // Write a program to find the length of a string using pointers.
+    // char userName[] = "Kailash Kumar";
+    // int strLength = findStrSize(userName);
+    // printf("The size of string is = %d\n", strLength);
+
+    // Implement a function to reverse a string using pointers.
+    // char userName[]= "Kailash";
+    // reverseStr(userName);
+    // printf("%s\n", userName); 
+
+    return 0;
+}
